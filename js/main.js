dc63a0f456bce713554109fb6e6e6b4fe9d88d38
Illia <ilr00743@gmail.com>
2021-03-30 13:05:00 +0300

    Add functions for menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("sidenavMenu").style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById("sidenavMenu").style.width = "0%";
+}
