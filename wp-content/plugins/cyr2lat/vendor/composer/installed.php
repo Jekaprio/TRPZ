b1016653bb896032487feb9d07747190728f0400
Illya Ryzhii <ilr00743@gmail.com>
2021-03-31 23:57:38 +0300

    Add plugins: ACF, cyr2lat, custom post type UI

---
+++ wp-content/plugins/cyr2lat/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '5.0.2',
+    'version' => '5.0.2.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b13080f905519be869babc177891b40de5b81b00',
+    'name' => 'mihdan/cyr2lat',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.9.0',
+      'version' => '1.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b93bcf0fa1fccb0b7d176b0967d969691cd74cca',
+    ),
+    'mihdan/cyr2lat' => 
+    array (
+      'pretty_version' => '5.0.2',
+      'version' => '5.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b13080f905519be869babc177891b40de5b81b00',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
--- wp-content/themes/litmap/functions.php
+++ wp-content/themes/litmap/functions.php
@@ -178,3 +178,27 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Remove default post type from admin panel
+ */
+
+//The Side Menu
+add_action( 'admin_menu', 'remove_default_post_type' );
+
+function remove_default_post_type() {
+	remove_menu_page( 'edit.php' );
+}
+
+// The +New Post in Admin Bar
+add_action( 'admin_bar_menu', 'remove_default_post_type_menu_bar', 999 );
+
+function remove_default_post_type_menu_bar( $wp_admin_bar ) {
+	$wp_admin_bar->remove_node( 'new-post' );
+}
+
+// The Quick Draft Dashboard Widget
+add_action( 'wp_dashboard_setup', 'remove_draft_widget', 999 );
+
+function remove_draft_widget(){
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+}
