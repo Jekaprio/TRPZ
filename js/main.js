35455971cba7636e49f25e435ff31c7c1f0a6f6f
Illya Ryzhii <ilr00743@gmail.com>
2021-03-31 22:31:37 +0300

    Just start to develop new website version

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function openMenu() {
-    document.getElementById("sidenavMenu").style.width = "100%";
-}
-
-function closeMenu() {
-    document.getElementById("sidenavMenu").style.width = "0%";
-}
